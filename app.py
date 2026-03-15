f5d6d18e21fb7de229dfeb42216a0642e97de4d2
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-15 10:10:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import os
 
 st.title("AI Options Trading Dashboard")
 
-client_id = os.getenv("DHAN_CLIENT_ID")
-access_token = os.getenv("DHAN_ACCESS_TOKEN")
+client_id = os.getenv("1106299230")
+access_token = os.getenv("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczNjM1OTc4LCJpYXQiOjE3NzM1NDk1NzgsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0._MqX20egfoTUbczsXOouL8PTBfoa8FkASXxoY_spTMGQUTvVOkV1OfxaQUu_7E-Z5eGGXClXFi1ap44wQDEQwQ")
 
 try:
     url = "https://api.dhan.co/v2/marketfeed/ltp"
