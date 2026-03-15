de1b6600d36f1502dbcbe6316e04be29f478eb9b
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-15 09:53:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,19 @@ import os
 
 st.title("AI Options Trading Dashboard")
 
-# Dhan credentials
 client_id = os.getenv("DHAN_CLIENT_ID")
 access_token = os.getenv("DHAN_ACCESS_TOKEN")
 
 try:
     dhan = dhanhq(client_id, access_token)
 
-    # Fetch NIFTY price
     data = dhan.get_market_feed({
         "NSE_EQ": [13]
     })
 
-    nifty_price = data["NSE_EQ"]["13"]["last_price"]
+    nifty = data["NSE_EQ"]["13"]["last_price"]
 
-    st.metric("NIFTY LTP", nifty_price)
+    st.metric("NIFTY LTP", nifty)
 
 except Exception as e:
     st.error(e)
