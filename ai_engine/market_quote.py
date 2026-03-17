93c521f5d02777b58885885b59dc032c4e36da46
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-17 10:01:04 +0530

    Update market_quote.py

--- ai_engine/market_quote.py
+++ ai_engine/market_quote.py
@@ -6,8 +6,8 @@ def get_market_quote():
     url = "https://api.dhan.co/v2/marketfeed/ltp"
 
     headers = {
-        "access-token": DHAN_ACCESS_TOKEN,
-        "client-id": DHAN_CLIENT_ID,
+        "access-token": eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczNjc3MTA0LCJpYXQiOjE3NzM1OTA3MDQsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.jnn6DApddObFVi53XcsMcMgEJGn8TgGyWL6ZjLs3sAYZ4dQUd4Ope8PZQ5Jy7rPPdKK3sIm9Tn5i7ZuXplziow,
+        "client-id": 1106299230,
         "Content-Type": "application/json"
     }
 
