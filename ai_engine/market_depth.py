ca1e12ec61ffb65298f4f255652a64affc9b3451
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-17 15:32:27 +0530

    Update market_depth.py

--- ai_engine/market_depth.py
+++ ai_engine/market_depth.py
@@ -1,21 +1,21 @@
 import requests
-from utils.config import DHAN_ACCESS_TOKEN, DHAN_CLIENT_ID
+import os
 
-def get_market_depth():
+def get_market_quote():
+    ACCESS_TOKEN = os.getenv("DHAN_ACCESS_TOKEN")
+    CLIENT_ID = os.getenv("DHAN_CLIENT_ID")
 
     url = "https://api.dhan.co/v2/marketfeed/ltp"
 
     headers = {
-        "access-token": eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODA5MTAwLCJpYXQiOjE3NzM3MjI3MDAsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.UVhOeBb2aD_qKfYfmm69icPXAlY7TT5FAhu1lNu3imOdXJyvfj5MR6FC8kwgXbvLS2I2Ix77tU0UX7ho0YpTkQ,
-        "client-id": 1106299230,
+        "access-token": ACCESS_TOKEN,
         "Content-Type": "application/json"
     }
 
     payload = {
-        "IDX_I": [13]   # ✅ FIX (Nifty)
+        "dhanClientId": CLIENT_ID,   # ✅ IMPORTANT
+        "IDX_I": [13, 25]
     }
 
-    try:
-        return requests.post(url, headers=headers, json=payload).json()
-    except Exception as e:
-        return {"error": str(e)}
+    res = requests.post(url, json=payload, headers=headers)
+    return res.json()
