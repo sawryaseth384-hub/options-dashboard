98e145c1d7ef9bbeaca494f215b4f392072ac292
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-17 15:20:51 +0530

    Update market_depth.py

--- ai_engine/market_depth.py
+++ ai_engine/market_depth.py
@@ -6,8 +6,8 @@ def get_market_depth():
     url = "https://api.dhan.co/v2/marketfeed/ltp"
 
     headers = {
-        "access-token": DHAN_ACCESS_TOKEN,
-        "client-id": DHAN_CLIENT_ID,
+        "access-token": eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODA5MTAwLCJpYXQiOjE3NzM3MjI3MDAsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.UVhOeBb2aD_qKfYfmm69icPXAlY7TT5FAhu1lNu3imOdXJyvfj5MR6FC8kwgXbvLS2I2Ix77tU0UX7ho0YpTkQ,
+        "client-id": 1106299230,
         "Content-Type": "application/json"
     }
 
