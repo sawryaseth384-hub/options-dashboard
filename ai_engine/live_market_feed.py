7c7d665250f0cd0b4bcedb9f2fa122ca7614d345
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-17 10:45:36 +0530

    Update live_market_feed.py

--- ai_engine/live_market_feed.py
+++ ai_engine/live_market_feed.py
@@ -1,1 +1,20 @@
+import requests
+from utils.config import DHAN_ACCESS_TOKEN, DHAN_CLIENT_ID
 
+def get_live_market_feed():
+
+    url = "https://api.dhan.co/v2/marketfeed/live"
+
+    headers = {
+        "access-token": DHAN_ACCESS_TOKEN,
+        "client-id": str(DHAN_CLIENT_ID)
+    }
+
+    payload = {
+        "securityId": ["13"],
+        "exchangeSegment": "NSE_FNO"
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    return response.json()
