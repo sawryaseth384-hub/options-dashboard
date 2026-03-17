6288890487c89da28e4116d3598759a49cbd25d2
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-17 09:11:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import streamlit as st
+
 from ai_engine.market_quote import get_market_quote
+from ai_engine.option_chain import get_option_chain
 
 st.title("AI Options Dashboard")
 
-data = get_market_quote()
+quote = get_market_quote()
+st.subheader("Market Quote")
+st.write(quote)
 
-st.write(data)
+option_data = get_option_chain()
+
+st.subheader("Option Chain Data")
+st.write(option_data)
