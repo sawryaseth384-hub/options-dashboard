afccf934fd93123f31d3f218ff246ef234c3c787
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-17 07:48:39 +0530

    Update data_fetcher.py

--- ai_engine/data_fetcher.py
+++ ai_engine/data_fetcher.py
@@ -1,1 +1,22 @@
+import requests
 
+DHAN_TOKEN = "YOUR_DHAN_API_TOKEN"
+
+def get_nifty_price():
+
+    url = "https://api.dhan.co/v2/marketfeed/ltp"
+
+    headers = {
+        "access-token": DHAN_TOKEN,
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "NSE_INDEX": ["Nifty 50"]
+    }
+
+    r = requests.post(url, json=payload, headers=headers)
+
+    data = r.json()
+
+    return data
