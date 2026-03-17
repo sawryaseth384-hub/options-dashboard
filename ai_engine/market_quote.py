2b8524906ce43c60de4f287cf3f65be04f851d1c
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-17 10:16:32 +0530

    Update market_quote.py

--- ai_engine/market_quote.py
+++ ai_engine/market_quote.py
@@ -6,7 +6,7 @@ def get_market_quote():
     url = "https://api.dhan.co/v2/marketfeed/ltp"
 
     headers = {
-        "access-token": eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczNjc3MTA0LCJpYXQiOjE3NzM1OTA3MDQsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.jnn6DApddObFVi53XcsMcMgEJGn8TgGyWL6ZjLs3sAYZ4dQUd4Ope8PZQ5Jy7rPPdKK3sIm9Tn5i7ZuXplziow,
+        "access-token": eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODA5MTAwLCJpYXQiOjE3NzM3MjI3MDAsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.UVhOeBb2aD_qKfYfmm69icPXAlY7TT5FAhu1lNu3imOdXJyvfj5MR6FC8kwgXbvLS2I2Ix77tU0UX7ho0YpTkQ,
         "client-id": 1106299230,
         "Content-Type": "application/json"
     }
