dafda16be447c84ba37262a9200015d14fe5f4c5
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 02:27:31 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,19 @@
-import streamlit as st
+# utils/config.py
 
-ACCESS_TOKEN = st.secrets.get("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODA5MTAwLCJpYXQiOjE3NzM3MjI3MDAsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.UVhOeBb2aD_qKfYfmm69icPXAlY7TT5FAhu1lNu3imOdXJyvfj5MR6FC8kwgXbvLS2I2Ix77tU0UX7ho0YpTkQ", "")
-CLIENT_ID = st.secrets.get("1106299230", "")
+import os
+
+def get_config():
+    access_token = os.getenv("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODA5MTAwLCJpYXQiOjE3NzM3MjI3MDAsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.UVhOeBb2aD_qKfYfmm69icPXAlY7TT5FAhu1lNu3imOdXJyvfj5MR6FC8kwgXbvLS2I2Ix77tU0UX7ho0YpTkQ")
+    client_id = os.getenv("1106299230")
+    errors = {}
+
+    if not access_token:
+        errors['ACCESS_TOKEN'] = "Missing ACCESS_TOKEN env"
+    if not client_id:
+        errors['CLIENT_ID'] = "Missing CLIENT_ID env"
+
+    return {
+        "ACCESS_TOKEN": access_token,
+        "CLIENT_ID": client_id,
+        "errors": errors
+    }
