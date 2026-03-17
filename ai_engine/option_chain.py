bfcd876ff71bf190de253d2a48c57c59b6d6b43d
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-17 09:10:45 +0530

    Update option_chain.py

--- ai_engine/option_chain.py
+++ ai_engine/option_chain.py
@@ -1,1 +1,21 @@
+import requests
+from utils.config import DHAN_ACCESS_TOKEN, DHAN_CLIENT_ID
 
+def get_option_chain():
+
+    url = "https://api.dhan.co/v2/optionchain"
+
+    headers = {
+        "access-token": DHAN_ACCESS_TOKEN,
+        "client-id": DHAN_CLIENT_ID,
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "UnderlyingScrip": "NIFTY",
+        "ExchangeSegment": "NSE_FNO"
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    return response.json()
