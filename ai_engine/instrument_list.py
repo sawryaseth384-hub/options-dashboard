194b55c2675298e23085d6ecdb874f00f9ce49b3
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-17 11:48:10 +0530

    Update instrument_list.py

--- ai_engine/instrument_list.py
+++ ai_engine/instrument_list.py
@@ -1,9 +1,24 @@
-import requests
+import pandas as pd
 
 def get_instrument_list():
 
-    url = "https://api.dhan.co/v2/instruments"
+    url = "https://images.dhan.co/api-data/api-scrip-master.csv"
 
-    response = requests.get(url)
+    try:
+        df = pd.read_csv(url)
 
-    return response.json()
+        # सिर्फ useful columns रखो
+        df = df[[
+            "SEM_EXM_EXCH_ID",
+            "SEM_SEGMENT",
+            "SEM_TRADING_SYMBOL",
+            "SEM_SM_SYMBOL_NAME",
+            "SEM_EXPIRY_DATE",
+            "SEM_STRIKE_PRICE",
+            "SEM_OPTION_TYPE"
+        ]]
+
+        return df.head(50).to_dict(orient="records")
+
+    except Exception as e:
+        return {"error": str(e)}
