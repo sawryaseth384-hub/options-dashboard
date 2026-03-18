65d1acf3a345f273077ecdb65be5b4523b8fb724
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 16:00:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 
+# IMPORT UI
 from dashboard.header import show_header
 from dashboard.navbar import show_navbar
+from dashboard.stocks import show_stocks
+from dashboard.options import show_options
+from dashboard.futures import show_futures
 
-st.set_page_config(page_title="Trading Dashboard", layout="wide")
+st.set_page_config(page_title="Pro Dashboard", layout="wide")
 
 # HEADER
 show_header()
@@ -11,7 +15,12 @@ show_header()
 # NAVBAR
 tab = show_navbar()
 
-# LOAD SCREEN
+# LOAD PAGE
 if tab == "Stocks":
-    from dashboard.stocks import show_stocks
     show_stocks()
+
+elif tab == "Options":
+    show_options()
+
+elif tab == "Futures":
+    show_futures()
