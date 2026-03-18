198f0a75ccc7d2411bee450709d13aa8a83f4c3b
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 07:50:50 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,19 @@
 import streamlit as st
 
-ACCESS_TOKEN = st.secrets.get("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODg1OTg5LCJpYXQiOjE3NzM3OTk1ODksInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.vRYxbB0OWqCIQ02J741rAoGugbSg3DV2bU1Ub-un-mAs-8QMzTFKCbnIp0RiC3AGASX4zVfmFS1nHRRYaYPmkQ", "")
-CLIENT_ID = st.secrets.get("1106299230", "")
+def get_config():
+    try:
+        return {
+            "ACCESS_TOKEN": st.secrets["eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODg1OTg5LCJpYXQiOjE3NzM3OTk1ODksInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.vRYxbB0OWqCIQ02J741rAoGugbSg3DV2bU1Ub-un-mAs-8QMzTFKCbnIp0RiC3AGASX4zVfmFS1nHRRYaYPmkQ"],
+            "CLIENT_ID": st.secrets["1106299230"]
+        }
+    except Exception as e:
+        return {
+            "ACCESS_TOKEN": "",
+            "CLIENT_ID": "",
+            "error": str(e)
+        }
+
+config = get_config()
+
+ACCESS_TOKEN = config["ACCESS_TOKEN"]
+CLIENT_ID = config["CLIENT_ID"]
