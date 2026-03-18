4f6a76b4a47f409dccb39e2ec69cab07f3c21cb3
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 08:47:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.title("API TEST")
 
 headers = {
-    "access-token": st.secrets["ACCESS_TOKEN"],
-    "client-id": st.secrets["CLIENT_ID"],
+    "access-token": st.secrets["eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODg1OTg5LCJpYXQiOjE3NzM3OTk1ODksInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.vRYxbB0OWqCIQ02J741rAoGugbSg3DV2bU1Ub-un-mAs-8QMzTFKCbnIp0RiC3AGASX4zVfmFS1nHRRYaYPmkQ"],
+    "client-id": st.secrets["1106299230"],
     "Content-Type": "application/json"
 }
 
