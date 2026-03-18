1991e55a8a66966af9315778d8ac7438a7bc0387
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 07:36:56 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,19 +1,2 @@
-# utils/config.py
-
-import os
-
-def get_config():
-    access_token = os.getenv("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODA5MTAwLCJpYXQiOjE3NzM3MjI3MDAsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.UVhOeBb2aD_qKfYfmm69icPXAlY7TT5FAhu1lNu3imOdXJyvfj5MR6FC8kwgXbvLS2I2Ix77tU0UX7ho0YpTkQ")
-    client_id = os.getenv("1106299230")
-    errors = {}
-
-    if not access_token:
-        errors['ACCESS_TOKEN'] = "Missing ACCESS_TOKEN env"
-    if not client_id:
-        errors['CLIENT_ID'] = "Missing CLIENT_ID env"
-
-    return {
-        "ACCESS_TOKEN": access_token,
-        "CLIENT_ID": client_id,
-        "errors": errors
-    }
+ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODg1OTg5LCJpYXQiOjE3NzM3OTk1ODksInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.vRYxbB0OWqCIQ02J741rAoGugbSg3DV2bU1Ub-un-mAs-8QMzTFKCbnIp0RiC3AGASX4zVfmFS1nHRRYaYPmkQ"
+CLIENT_ID = "1106299230"
