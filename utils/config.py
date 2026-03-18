c5f6dfc66029876a0c9c7b6bc9f8c2fef69c6d0b
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 07:53:56 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -3,8 +3,8 @@ import streamlit as st
 def get_config():
     try:
         return {
-            "ACCESS_TOKEN": st.secrets["eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzczODg1OTg5LCJpYXQiOjE3NzM3OTk1ODksInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA2Mjk5MjMwIn0.vRYxbB0OWqCIQ02J741rAoGugbSg3DV2bU1Ub-un-mAs-8QMzTFKCbnIp0RiC3AGASX4zVfmFS1nHRRYaYPmkQ"],
-            "CLIENT_ID": st.secrets["1106299230"]
+            "ACCESS_TOKEN": st.secrets["ACCESS_TOKEN"],
+            "CLIENT_ID": st.secrets["CLIENT_ID"]
         }
     except Exception as e:
         return {
