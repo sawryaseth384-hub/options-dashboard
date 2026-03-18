d4366bc86ee51c66307838b080aac535a4efeb4c
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 07:52:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ from utils.config import ACCESS_TOKEN, CLIENT_ID
 
 st.write("TOKEN LENGTH:", len(ACCESS_TOKEN))
 st.write("CLIENT ID:", CLIENT_ID)
+from utils.config import config
+import streamlit as st
+
+st.write("CONFIG:", config)
