250eb1da22955133cf05e75d817129bf9daa0fb2
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 20:30:41 +0530

    Update api.py

--- core/api.py
+++ core/api.py
@@ -1,23 +1,25 @@
-import requests
-from utils.config import get_keys
+# core/api.py
 
 def fetch_data():
 
-    CLIENT_ID, ACCESS_TOKEN = get_keys()
+    # 👉 अभी dummy (replace with your Dhan API)
+    return {
+        "NIFTY": {"ltp": 23700, "change": 120},
+        "BANKNIFTY": {"ltp": 55200, "change": 300},
+        "SENSEX": {"ltp": 78500, "change": 250},
+        "VIX": {"ltp": 18.2, "change": -0.5},
 
-    url = "https://api.dhan.co/v2/marketfeed/quote"
+        "DOW": {"ltp": 38000, "change": 200},
+        "NASDAQ": {"ltp": 16500, "change": -50},
+        "GIFT": {"ltp": 23750, "change": 80},
 
-    headers = {
-        "access-token": ACCESS_TOKEN,
-        "client-id": CLIENT_ID,
-        "Content-Type": "application/json"
+        "CRUDE": {"ltp": 6500, "change": 30},
+        "GOLD": {"ltp": 72000, "change": 100},
+        "SILVER": {"ltp": 85000, "change": 200},
+
+        "USDINR": {"ltp": 83.1, "change": 0.1},
+        "DXY": {"ltp": 104.2, "change": -0.2},
+
+        "FII": 1200,
+        "PCR": 1.2
     }
-
-    payload = {
-        "NSE_EQ": [11536],
-        "NSE_FNO": [49081]
-    }
-
-    res = requests.post(url, headers=headers, json=payload)
-
-    return res.json()
