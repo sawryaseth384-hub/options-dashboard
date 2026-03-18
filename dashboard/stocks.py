ecdf423fa14f0d4d17f40b64b67a4452573ef621
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 20:38:04 +0530

    Update stocks.py

--- dashboard/stocks.py
+++ dashboard/stocks.py
@@ -1,22 +1,5 @@
 import streamlit as st
-import pandas as pd
 
-def show_stocks():
-
+def show_stocks(data):
     st.subheader("📊 Stocks")
-
-    col1, col2 = st.columns([3, 1])
-
-    with col1:
-        df = pd.DataFrame({
-            "Stock": ["RELIANCE", "TCS", "INFY"],
-            "Price": [2500, 3500, 1500],
-            "Change": ["+10", "-20", "+15"]
-        })
-
-        st.dataframe(df, use_container_width=True)
-
-    with col2:
-        st.markdown("### ⚙️ Filters")
-        st.selectbox("Stock", ["RELIANCE", "TCS", "INFY"])
-        st.button("Apply")
+    st.write(data["NIFTY"])
