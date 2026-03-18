633667565a8c1cacb97a4905e6e71f0ca5fa0a46
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 21:05:26 +0530

    Update header.py

--- dashboard/header.py
+++ dashboard/header.py
@@ -2,42 +2,75 @@ import streamlit as st
 
 def show_header(data):
 
-    def item(name, ltp, chg):
-        color = "#00c853" if chg >= 0 else "#ff1744"
-        arrow = "▲" if chg >= 0 else "▼"
-
-        return f"""
-        <span style='margin-right:22px; font-size:13px; color:#bbb'>
-            <b style='color:white'>{name}</b> {ltp}
-            <span style='color:{color}'> {arrow} {chg}</span>
-        </span>
-        """
-
     html = f"""
     <div style='
         background:#0b1220;
-        padding:10px 20px;
-        border-radius:10px;
-        margin-bottom:15px;
+        padding:10px 18px;
+        border-radius:8px;
+        margin-bottom:10px;
         overflow-x:auto;
         white-space:nowrap;
     '>
 
-    {item("NIFTY", data["NIFTY"]["ltp"], data["NIFTY"]["change"])}
-    {item("BANKNIFTY", data["BANKNIFTY"]["ltp"], data["BANKNIFTY"]["change"])}
-    {item("SENSEX", data["SENSEX"]["ltp"], data["SENSEX"]["change"])}
-    {item("VIX", data["VIX"]["ltp"], data["VIX"]["change"])}
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>NIFTY</b> {data["NIFTY"]["ltp"]}
+        <span style='color:#00c853'> ▲ {data["NIFTY"]["change"]}</span>
+    </span>
 
-    {item("DOW", data["DOW"]["ltp"], data["DOW"]["change"])}
-    {item("NASDAQ", data["NASDAQ"]["ltp"], data["NASDAQ"]["change"])}
-    {item("GIFT", data["GIFT"]["ltp"], data["GIFT"]["change"])}
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>BANKNIFTY</b> {data["BANKNIFTY"]["ltp"]}
+        <span style='color:#00c853'> ▲ {data["BANKNIFTY"]["change"]}</span>
+    </span>
 
-    {item("CRUDE", data["CRUDE"]["ltp"], data["CRUDE"]["change"])}
-    {item("GOLD", data["GOLD"]["ltp"], data["GOLD"]["change"])}
-    {item("SILVER", data["SILVER"]["ltp"], data["SILVER"]["change"])}
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>SENSEX</b> {data["SENSEX"]["ltp"]}
+        <span style='color:#00c853'> ▲ {data["SENSEX"]["change"]}</span>
+    </span>
 
-    {item("USDINR", data["USDINR"]["ltp"], data["USDINR"]["change"])}
-    {item("DXY", data["DXY"]["ltp"], data["DXY"]["change"])}
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>VIX</b> {data["VIX"]["ltp"]}
+        <span style='color:#ff1744'> ▼ {data["VIX"]["change"]}</span>
+    </span>
+
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>DOW</b> {data["DOW"]["ltp"]}
+        <span style='color:#00c853'> ▲ {data["DOW"]["change"]}</span>
+    </span>
+
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>NASDAQ</b> {data["NASDAQ"]["ltp"]}
+        <span style='color:#ff1744'> ▼ {data["NASDAQ"]["change"]}</span>
+    </span>
+
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>GIFT</b> {data["GIFT"]["ltp"]}
+        <span style='color:#00c853'> ▲ {data["GIFT"]["change"]}</span>
+    </span>
+
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>CRUDE</b> {data["CRUDE"]["ltp"]}
+        <span style='color:#00c853'> ▲ {data["CRUDE"]["change"]}</span>
+    </span>
+
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>GOLD</b> {data["GOLD"]["ltp"]}
+        <span style='color:#00c853'> ▲ {data["GOLD"]["change"]}</span>
+    </span>
+
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>SILVER</b> {data["SILVER"]["ltp"]}
+        <span style='color:#00c853'> ▲ {data["SILVER"]["change"]}</span>
+    </span>
+
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>USDINR</b> {data["USDINR"]["ltp"]}
+        <span style='color:#00c853'> ▲ {data["USDINR"]["change"]}</span>
+    </span>
+
+    <span style='margin-right:22px; font-size:13px; color:#bbb'>
+        <b style='color:white'>DXY</b> {data["DXY"]["ltp"]}
+        <span style='color:#ff1744'> ▼ {data["DXY"]["change"]}</span>
+    </span>
 
     </div>
     """
