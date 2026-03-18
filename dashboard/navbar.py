ba65422b250642f20c580cea98003933acce5efa
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-18 20:32:41 +0530

    Update navbar.py

--- dashboard/navbar.py
+++ dashboard/navbar.py
@@ -1,10 +1,12 @@
+# dashboard/navbar.py
+
 import streamlit as st
 
 def show_navbar():
 
     tab = st.radio(
         "",
-        ["Stocks", "Options", "Futures"],
+        ["Stocks", "AI Options", "Futures"],
         horizontal=True
     )
 
