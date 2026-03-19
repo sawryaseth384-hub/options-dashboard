f57d63cedae857479983acce58108404d920e75a
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-19 12:26:39 +0530

    Update api.py

--- core/api.py
+++ core/api.py
@@ -1,16 +1,31 @@
-def fetch_data():
-    return [
-        {"name": "NIFTY", "price": 23700, "change": 120},
-        {"name": "BANKNIFTY", "price": 55200, "change": 300},
-        {"name": "SENSEX", "price": 78500, "change": 250},
-        {"name": "VIX", "price": 18.2, "change": -0.5},
+import random
 
-        {"name": "DOW", "price": 38000, "change": 200},
-        {"name": "NASDAQ", "price": 16500, "change": -50},
-        {"name": "GIFT", "price": 23750, "change": 80},
-        {"name": "CRUDE", "price": 6500, "change": 30},
-        {"name": "GOLD", "price": 72000, "change": 100},
-        {"name": "SILVER", "price": 85000, "change": 200},
-        {"name": "USDINR", "price": 83.1, "change": 0.1},
-        {"name": "DXY", "price": 104.2, "change": -0.2},
-    ]
+# base data (initial)
+base_data = [
+    {"name": "NIFTY", "price": 23700},
+    {"name": "BANKNIFTY", "price": 55200},
+    {"name": "SENSEX", "price": 78500},
+    {"name": "VIX", "price": 18.2},
+    {"name": "DOW", "price": 38000},
+    {"name": "NASDAQ", "price": 16500},
+    {"name": "GIFT", "price": 23750},
+    {"name": "CRUDE", "price": 6500},
+    {"name": "GOLD", "price": 72000},
+    {"name": "SILVER", "price": 85000},
+    {"name": "USDINR", "price": 83.1},
+    {"name": "DXY", "price": 104.2},
+]
+
+def fetch_data():
+    data = []
+
+    for item in base_data:
+        change = round(random.uniform(-100, 100), 2)
+
+        data.append({
+            "name": item["name"],
+            "price": round(item["price"] + change, 2),
+            "change": change
+        })
+
+    return data
