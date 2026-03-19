593b1da6e04a08f45a18b478bfdd4da00c0ae988
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-19 10:23:04 +0530

    Update api.py

--- core/api.py
+++ core/api.py
@@ -1,5 +1,18 @@
 def fetch_data():
     return [
-        {"name":"NIFTY","price":23700,"change":120},
-        {"name":"BANKNIFTY","price":55200,"change":300},
+        {"name": "NIFTY", "price": 23700, "change": 120},
+        {"name": "BANKNIFTY", "price": 55200, "change": 300},
+        {"name": "SENSEX", "price": 78500, "change": 250},
+        {"name": "VIX", "price": 18.2, "change": -0.5},
+
+        {"name": "DOW", "price": 38000, "change": 200},
+        {"name": "NASDAQ", "price": 16500, "change": -50},
+        {"name": "GIFT", "price": 23750, "change": 80},
+
+        {"name": "CRUDE", "price": 6500, "change": 30},
+        {"name": "GOLD", "price": 72000, "change": 100},
+        {"name": "SILVER", "price": 85000, "change": 200},
+
+        {"name": "USDINR", "price": 83.1, "change": 0.1},
+        {"name": "DXY", "price": 104.2, "change": -0.2},
     ]
