7690ff508537d958faaecb796385e9aa942959d1
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-19 12:27:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st
+import time
 from core.api import fetch_data
 from dashboard.header import show_header
 
-st.set_page_config(page_title="Dhan Pro Dashboard", layout="wide")
+st.set_page_config(page_title="Dhan Live Dashboard", layout="wide")
 
-data = fetch_data()
+# 🔥 AUTO REFRESH LOOP
+placeholder = st.empty()
 
-show_header(data)
+while True:
+    data = fetch_data()
+
+    with placeholder.container():
+        show_header(data)
+
+    time.sleep(2)   # हर 2 सेकंड refresh
