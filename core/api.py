150a0d1eb0a136bbb728a8a3e4522251cd2e3eec
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-19 04:17:13 +0000

    Update api.py

--- core/api.py
+++ core/api.py
@@ -1,23 +1,19 @@
 def fetch_data():
-    return {
-        "indices": {
-            "nifty": {"ltp": 23700, "change": 120},
-            "banknifty": {"ltp": 55200, "change": 300},
-            "sensex": {"ltp": 78500, "change": 250},
-            "vix": {"ltp": 18.2, "change": -0.5},
-        },
-        "global": {
-            "dow": {"ltp": 38000, "change": 200},
-            "nasdaq": {"ltp": 16500, "change": -50},
-            "gift": {"ltp": 23750, "change": 80},
-        },
-        "commodities": {
-            "crude": {"ltp": 6500, "change": 30},
-            "gold": {"ltp": 72000, "change": 100},
-            "silver": {"ltp": 85000, "change": 200},
-        },
-        "currency": {
-            "usd": {"ltp": 83.1, "change": 0.1},
-            "dxy": {"ltp": 104.2, "change": -0.2},
-        }
-    }
+
+    return [
+        {"name": "NIFTY", "ltp": 23700, "change": 120},
+        {"name": "BANKNIFTY", "ltp": 55200, "change": 300},
+        {"name": "SENSEX", "ltp": 78500, "change": 250},
+        {"name": "VIX", "ltp": 18.2, "change": -0.5},
+
+        {"name": "DOW", "ltp": 38000, "change": 200},
+        {"name": "NASDAQ", "ltp": 16500, "change": -50},
+        {"name": "GIFT", "ltp": 23750, "change": 80},
+
+        {"name": "CRUDE", "ltp": 6500, "change": 30},
+        {"name": "GOLD", "ltp": 72000, "change": 100},
+        {"name": "SILVER", "ltp": 85000, "change": 200},
+
+        {"name": "USDINR", "ltp": 83.1, "change": 0.1},
+        {"name": "DXY", "ltp": 104.2, "change": -0.2},
+    ]
