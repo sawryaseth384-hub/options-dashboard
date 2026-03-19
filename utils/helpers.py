27e9629817710517478193e9729bd261735bc9ea
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-19 22:22:57 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,30 +1,33 @@
 import pandas as pd
 
 def process_option_data(response):
-    if not response or "data" not in response:
+    if not response or response.get("status") != "success":
         return pd.DataFrame(), 0
 
     data = response["data"]
-
     spot = data.get("last_price", 0)
     oc = data.get("oc", {})
 
     rows = []
 
-    for strike, val in oc.items():
-        ce = val.get("ce", {})
-        pe = val.get("pe", {})
+    for strike_str, options in oc.items():
+        strike = float(strike_str)
+
+        ce = options.get("ce", {})
+        pe = options.get("pe", {})
 
         rows.append({
-            "Strike": float(strike),
+            "Strike": strike,
 
             "Call OI": ce.get("oi", 0),
             "Call LTP": ce.get("last_price", 0),
             "Call IV": ce.get("implied_volatility", 0),
+            "Call Delta": ce.get("greeks", {}).get("delta", 0),
 
             "Put OI": pe.get("oi", 0),
             "Put LTP": pe.get("last_price", 0),
             "Put IV": pe.get("implied_volatility", 0),
+            "Put Delta": pe.get("greeks", {}).get("delta", 0),
         })
 
     df = pd.DataFrame(rows).sort_values("Strike")
