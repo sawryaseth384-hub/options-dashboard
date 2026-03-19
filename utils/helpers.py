f86a39903e8d47983e015acbc8c15e8554aa0ca0
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-20 01:03:11 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,35 +1,33 @@
 import pandas as pd
 
-def process_option_data(response):
-    if not response or response.get("status") != "success":
+def process_option_data(raw_data):
+    try:
+        data = raw_data.get("data", {})
+        spot = data.get("last_price", 0)
+
+        oc = data.get("oc", {})
+
+        rows = []
+
+        for strike, values in oc.items():
+            ce = values.get("ce", {})
+            pe = values.get("pe", {})
+
+            rows.append({
+                "Strike": float(strike),
+
+                "CE OI": ce.get("oi", 0),
+                "CE LTP": ce.get("last_price", 0),
+                "CE Delta": ce.get("greeks", {}).get("delta", 0),
+
+                "PE OI": pe.get("oi", 0),
+                "PE LTP": pe.get("last_price", 0),
+                "PE Delta": pe.get("greeks", {}).get("delta", 0),
+            })
+
+        df = pd.DataFrame(rows)
+
+        return df.sort_values("Strike"), spot
+
+    except Exception:
         return pd.DataFrame(), 0
-
-    data = response["data"]
-    spot = data.get("last_price", 0)
-    oc = data.get("oc", {})
-
-    rows = []
-
-    for strike_str, options in oc.items():
-        strike = float(strike_str)
-
-        ce = options.get("ce", {})
-        pe = options.get("pe", {})
-
-        rows.append({
-            "Strike": strike,
-
-            "Call OI": ce.get("oi", 0),
-            "Call LTP": ce.get("last_price", 0),
-            "Call IV": ce.get("implied_volatility", 0),
-            "Call Delta": ce.get("greeks", {}).get("delta", 0),
-
-            "Put OI": pe.get("oi", 0),
-            "Put LTP": pe.get("last_price", 0),
-            "Put IV": pe.get("implied_volatility", 0),
-            "Put Delta": pe.get("greeks", {}).get("delta", 0),
-        })
-
-    df = pd.DataFrame(rows).sort_values("Strike")
-
-    return df, spot
