c3e24e14bac16e218580434ced9aa041cdd67431
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-20 14:12:46 +0530

    Update instruments.py

--- dhan_data/instruments.py
+++ dhan_data/instruments.py
@@ -1,13 +1,22 @@
 import pandas as pd
+import streamlit as st
+
+URL = "https://images.dhan.co/api-data/api-scrip-master.csv"
+
+@st.cache_data(ttl=3600)
+def load_instruments():
+    df = pd.read_csv(URL)
+
+    # 🔥 Only NSE + Equity/Derivatives
+    df = df[df["SEM_EXM_EXCH_ID"] == "NSE"]
+
+    return df
+
 
 def get_instrument_list():
+    df = load_instruments()
 
-    url = "https://images.dhan.co/api-data/api-scrip-master.csv"
+    # 🔥 Unique symbols
+    symbols = df["SEM_TRADING_SYMBOL"].dropna().unique().tolist()
 
-    try:
-        df = pd.read_csv(url)
-
-        return df.head(10).to_dict(orient="records")
-
-    except Exception as e:
-        return {"error": str(e)}
+    return sorted(symbols)
