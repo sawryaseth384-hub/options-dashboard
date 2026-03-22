c184d4c9bf6361db5a53003f38624574f1f74bde
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-22 21:50:42 +0530

    Update totp.py

--- core/totp.py
+++ core/totp.py
@@ -1,7 +1,7 @@
 import pyotp
 import streamlit as st
 
-TOTP_SECRET = st.secrets["TOTP_SECRET"]
+TOTP_SECRET = st.secrets["DJUQ7WLHTV2ZVFHOTOORRT3VGHQJCMLV"]
 
 def get_totp():
     return pyotp.TOTP(TOTP_SECRET).now()
