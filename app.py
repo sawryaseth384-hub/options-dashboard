c0a6655bee63938d6bfa674e1f745e350a4d89f1
sawryaseth384-hub <sawryaseth384@gmail.com>
2026-03-23 16:55:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,14 @@
-import streamlit as st
+import requests
 from core.token_manager import get_headers
-from expiry import get_expiry
-from option_chain import get_option_chain
 
-st.title("📊 Dhan Options Dashboard")
+BASE_URL = "https://api.dhan.co/v2"
 
-symbol = st.text_input("Symbol", "NIFTY")
+def get_expiry(security_id):
+    url = f"{BASE_URL}/optionchain/expirylist"
 
-# ✅ fixed ID
-SECURITY_ID = 13
+    payload = {
+        "UnderlyingScrip": int(security_id),
+        "UnderlyingSeg": "IDX_I"
+    }
 
-st.write("🔍 NIFTY security ID:", SECURITY_ID)
-
-# 🔥 Step 1: Expiry
-expiry_list = get_expiry(SECURITY_ID)
-
-if expiry_list:
-    expiry = st.selectbox("Select Expiry", expiry_list)
-
-    # 🔥 Step 2: Option Chain
-    data = get_option_chain(SECURITY_ID, expiry)
-
-    st.subheader("Option Chain Raw Response")
-    st.json(data)
-
-    if "data" in data:
-        st.success("✅ Option Chain Loaded")
-    else:
-        st.warning("Option chain empty")
-
-else:
-    st.error("❌ Expiry not loaded")
+    res = requests.post(url
